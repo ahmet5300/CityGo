0a0deca09e3bd78dfd98001770c7032bb3fad8df
Ahmet5300 <25asahin25@gmail.com>
2017-04-19 11:41:37 +0200

    voice

--- app/src/main/java/com/example/a25ahm/citygo/MainActivity.java
+++ app/src/main/java/com/example/a25ahm/citygo/MainActivity.java
@@ -29,7 +29,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-       Button btnSpeak = (Button) findViewById(R.id.btnSpeak);
+        Button btnSpeak = (Button) findViewById(R.id.btnSpeak);
+        txtSpeechInput = (TextView) findViewById(R.id.textView);
 
         btnSpeak.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -40,6 +41,7 @@ public class MainActivity extends AppCompatActivity {
 
 
     }
+
     private void promptSpeechInput() {
         Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
         intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL,
@@ -59,9 +61,9 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         ArrayList<String> Museacomments = new ArrayList<>(Arrays.asList("start musea", "musea", "museaum"));
-        ArrayList<String> helpCommands = new ArrayList<>(Arrays.asList("hint", "geef een hint", "tip", "geef een tip"));
-        ArrayList<String> stopCommands = new ArrayList<>(Arrays.asList("stop", "stop speurtocht"));
-        ArrayList<String> bezichtigingCommands = new ArrayList<>(Arrays.asList("meer informatie", "informatie"));
+        ArrayList<String> Bezienswaardighedencomments = new ArrayList<>(Arrays.asList("Bezienswaardigheid", "bezien", "beziens"));
+        ArrayList<String> Restaurantsdisplay = new ArrayList<>(Arrays.asList("Restaurant", "Restaur", "Res"));
+        ArrayList<String> cafescomment = new ArrayList<>(Arrays.asList("cafe", "cafes"));
         super.onActivityResult(requestCode, resultCode, data);
 
         switch (requestCode) {
@@ -74,64 +76,57 @@ public class MainActivity extends AppCompatActivity {
                     txtSpeechInput.setText(result.get(0));
 
 
-                        if (Museacomments.contains(result.get(0))) {
-                            but3(View.);
-
-                        if (completedSpeurtocht) {
-                            if (bezichtigingCommands.contains(result.get(0))) {
-
-                            }
-                        }
+                    if (Museacomments.contains(result.get(0))) {
+                        but4(findViewById(R.id.button4));
                     }
-                    if (runningSpeurtocht) {
-                        if (helpCommands.contains(result.get(0))) {
-
-                        }
-                        if (stopCommands.contains(result.get(0))) {
-                            runningSpeurtocht = false;
-                            t1.speak("Quest stopped", TextToSpeech.QUEUE_FLUSH, null);
-                            radiusSeekBar.setVisibility(View.VISIBLE);
-                            fab.setImageResource(R.drawable.ic_media_pause);
-                        }
+                    if (Bezienswaardighedencomments.contains(result.get(0))) {
+                        But1(findViewById(R.id.button1));
                     }
+
+                    if (Restaurantsdisplay.contains(result.get(0))) {
+                        but2(findViewById(R.id.button2));
+                    }
+
+                    if (cafescomment.contains(result.get(0))) {
+                        but3(findViewById(R.id.button3));
+                    }
+
                 }
-                break;
-            }
 
+            }
+            break;
         }
+
     }
 
-    public void  But1 (View v){
-        if (v.getId() == R.id.button1)
-        {
-            Intent b1 = new Intent(MainActivity.this,BezienswaardighedenDisplay.class);
+
+    public void But1(View v) {
+        if (v.getId() == R.id.button1) {
+            Intent b1 = new Intent(MainActivity.this, BezienswaardighedenDisplay.class);
             startActivity(b1);
 
         }
     }
 
-    public void  but2 (View v){
-        if (v.getId() == R.id.button2)
-        {
-            Intent b2 = new Intent(MainActivity.this,RestaurantsDisplay.class);
+    public void but2(View v) {
+        if (v.getId() == R.id.button2) {
+            Intent b2 = new Intent(MainActivity.this, RestaurantsDisplay.class);
             startActivity(b2);
 
         }
     }
 
-    public void  but3 (View v){
-        if (v.getId() == R.id.button3)
-        {
-            Intent b3 = new Intent(MainActivity.this,CafesDisplay.class);
+    public void but3(View v) {
+        if (v.getId() == R.id.button3) {
+            Intent b3 = new Intent(MainActivity.this, CafesDisplay.class);
             startActivity(b3);
 
         }
     }
 
-    public void  but4 (View v){
-        if (v.getId() == R.id.button4)
-        {
-            Intent b4 = new Intent(MainActivity.this,MuseaDisplay.class);
+    public void but4(View v) {
+        if (v.getId() == R.id.button4) {
+            Intent b4 = new Intent(MainActivity.this, MuseaDisplay.class);
             startActivity(b4);
 
         }
